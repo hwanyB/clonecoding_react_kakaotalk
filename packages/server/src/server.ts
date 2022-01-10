a9594a39c0efa40e0594e5240d6a2703250f16b2
hwanyb <mnn9502@naver.com>
2022-01-10 11:00:00 +0900

    install express-session / session-file-store / session 설정

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,8 +1,20 @@
 import express, { Application } from 'express';
+import session from 'express-session';
+
 import sequelize from './sequelize';
 import routes from './routes';
 
+const FileStore = require('session-file-store')(session);
 const app: Application = express();
+const sessionMidleware = session({
+    secret: 'kkokkiotalk',
+    saveUninitialized: true,
+    cookie: { secure: false },
+    resave: false,
+    store: new FileStore(),
+});
+
+app.use(sessionMidleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
