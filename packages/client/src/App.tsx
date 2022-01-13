70f770c615c5961793a8c2e2e861706f4c46feb5
hwanyb <mnn9502@naver.com>
2022-01-13 11:00:00 +0900

    [client] Setting Router

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Friends from './pages/Friends';
+import Lobby from './pages/Lobby';
+import RoomDetail from './pages/RoomDetail';
+import RoomList from './pages/RoomList';
+import SeeMore from './pages/SeeMore';
 
 function App() {
   return (
-    <div>Hello Kkokkiotalk</div>
+    <Routes>
+      <Route index element={<Lobby />} /> {/* Lobby */}
+      <Route path="/friends" element={<Friends />} /> {/* Friends */}
+      <Route path="/rooms" element={<RoomList />} /> {/* RoomList */}
+      <Route path="/rooms/:roomId" element={<RoomDetail />} /> {/* RoomDetail */}
+      <Route path="/more" element={<SeeMore />} /> {/* Seemore */}
+    </Routes>
   );
 }
 
---
+++ packages/client/src/pages/Lobby.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Lobby: React.FC = () => {
+    return (
+        <div>
+            Looby
+        </div>
+    )
+}
+
+export default Lobby;
