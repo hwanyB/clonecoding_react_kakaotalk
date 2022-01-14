4ce2803b5d43903945869d7eabc21d710d96cfda
hwanyb <mnn9502@naver.com>
2022-01-14 11:00:00 +0900

    [client] Components styling - Seemore>IconButtonList

--- packages/client/src/components/SeeMore/IconButtonList.tsx
+++ packages/client/src/components/SeeMore/IconButtonList.tsx
@@ -1,18 +1,56 @@
 import React from 'react';
 import styled from '@emotion/styled/macro';
+import { FiMail, FiCalendar, FiScissors, FiGift } from 'react-icons/fi'
 
-// const Base = styled.div``;
-// const ImageWrapper = styled.div``;
-// const Image = styled.img``;
-// const Info = styled.div``;
-// const UserName = styled.p``;
-// const PhoneNumber = styled.p``;
+const Base = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 3fr);
+    width: 100%;
+    margin-top: 36px;
+`;
+
+const IconButton = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+const Icon = styled.div`
+    font-size: 24px;
+`;
+const Label = styled.div`
+    margin-top: 4px;
+`;
 
 
 
 const IconButtonList:React.FC = () => {
     return (
-        <></>
+        <Base>
+            <IconButton>
+                <Icon>
+                    <FiMail />
+                </Icon>
+                <Label>메일</Label>
+            </IconButton>
+            <IconButton>
+                <Icon>
+                    <FiCalendar />
+                </Icon>
+                <Label>캘린더</Label>
+            </IconButton>
+            <IconButton>
+                <Icon>
+                    <FiScissors />
+                </Icon>
+                <Label>헤어샵</Label>
+            </IconButton>
+            <IconButton>
+                <Icon>
+                    <FiGift />
+                </Icon>
+                <Label>선물</Label>
+            </IconButton>
+        </Base>
     )
 }
 
--- packages/client/src/pages/SeeMore.tsx
+++ packages/client/src/pages/SeeMore.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BottomNavigation from '../components/BottomNavigation';
+import IconButtonList from '../components/SeeMore/IconButtonList';
 import UserInfo from '../components/SeeMore/UserInfo';
 import TopNavigation from '../components/TopNavigation';
 
@@ -8,6 +9,7 @@ const SeeMore: React.FC = () => {
         <div>
             <TopNavigation title='더보기' />
             <UserInfo telNo='010-3050-3337' username='변환희' />
+            <IconButtonList />
             <BottomNavigation />
         </div>
     )
