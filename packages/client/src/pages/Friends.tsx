3f4e3b527d87530ea9cfbc214186a514e05ea931
hwanyb <mnn9502@naver.com>
2022-01-19 11:00:00 +0900

    [client] Component design - Friend List

--- packages/client/src/pages/Friends.tsx
+++ packages/client/src/pages/Friends.tsx
@@ -1,14 +1,91 @@
 import React from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import styled from '@emotion/styled/macro';
+
 import BottomNavigation from '../components/BottomNavigation';
 import TopNavigation from '../components/TopNavigation';
+import Profile from '../components/Profile';
+import FriendList from '../components/FriendList';
+import { useMutation, useQuery } from 'react-query';
+import { fetchMyProfile, fetchUserList } from '../apis/userApi';
+import { AxiosError, AxiosResponse } from 'axios';
+import Friend from '../components/FriendList/Friend';
+import { makeChatRoom, MakeChatRoomRequest } from '../apis/roomApi';
+
+const Base = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 0 21px;
+  box-sizing: border-box;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Summary = styled.small`
+  margin: 4px 0;
+  padding: 24px 0 0 0;
+  font-size: 12px;
+`;
 
 const Friends: React.FC = () => {
+    const navigate = useNavigate();
+
+    const { data: chatRoomListData } = useQuery<AxiosResponse<Array<Iroom>>, AxiosError>('fetchChatRoomList', fetchChatRoomList);
+
+    const { data: profileData } = useQuery<AxiosResponse<IProfile>, AxiosError>('fetchMyProfile', fetchMyProfile);
+
+    const { data: userData } = useQuery<AxiosResponse<{ count: number, rows: Array<IUser> }>, AxiosError>('fetchUserList', fetchUserList);
+
+    const mutation = useMutation('makeChatRoom', (request: MakeChatRoomRequest)=>
+        makeChatRoom(request)
+    );
+
+    const handleChatRoomCreate = (opponentId: string) => {
+        const chatRoom = chatRoomListData?.data.find(chatRoom => chatRoom.chatRoomId === opponentId);
+        if (chatRoom) {
+            navigate(`/rooms/${chatRoom.id}`);
+        } else {
+            mutation.mutate({
+                opponentId
+            }, {
+                onSuccess: (data) => {
+                    navigate(`/room/${data.data.id}`)
+                }
+            });
+        }
+    }
+
     return (
-        <div>
-            <TopNavigation title='친구' />
-            Friends
-            <BottomNavigation />
-        </div>
+        <Base>
+            <Container>
+                <TopNavigation title='친구' />
+                {
+                    profileData && <Profile username={profileData.data.username} />
+                }
+                {
+                    userData && (
+                        <>
+                            <Summary> 친구 : {userData.data.count}</Summary>
+                        </>
+                    )
+                }
+                <FriendList>
+                    {
+                        userData.data.rows.map(row => (
+                            <Friend
+                            key={row.id}
+                            username={row.username}
+                            thumbnailImage={row.thumbnailImageUrl}
+                            onClick={() => handleChatRoomCreate(row.id)} />
+                        ))
+                    }
+                </FriendList>
+                <BottomNavigation />
+            </Container>
+        </Base>
     )
 }
 
