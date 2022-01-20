b4fa03f0a775086c6276b459a8993b813a8bef6b
hwanyb <mnn9502@naver.com>
2022-01-20 21:40:02 +0900

    [client] Component design - Chat Room Detail(Sent Message)

--- packages/client/src/components/ChatRoomDetail/MessageList.tsx
+++ packages/client/src/components/ChatRoomDetail/MessageList.tsx
@@ -1,13 +1,27 @@
-// import React from 'react';
-// import styled from '@emotion/styled/macro';
+import React from 'react';
+import styled from '@emotion/styled/macro';
 
-// const MessageList: React.FC = () => {
-//   return (
-//       <></>
-//   )
-// }
 export interface MessageType {
     senderId: string;
     content: string;
     timestamp: string
 }
+
+const Base = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0 0 48px 0;
+    width: 100%;
+    > li + li {
+        margin-top: 25px;
+    }
+`;
+
+const MessageList: React.FC = ({ children }) => {
+    return (
+        <Base>{children}</Base>
+
+    )
+}
+
+export default MessageType;
--- packages/client/src/components/ChatRoomDetail/SentMessage.tsx
+++ packages/client/src/components/ChatRoomDetail/SentMessage.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import styled from '@emotion/styled/macro';
+import { useTheme } from '@emotion/react';
+import { MessageType } from './MessageList';
 
-const SentMessage: React.FC = () => {
-  return (
-      <></>
-  )
+const Base = styled.li`
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: flex-end;
+    width: 100%;
+`;
+
+const SpeechBubble = styled.span<{ backgroundColor: string }>`
+    background-color: ${({backgroundColor}) => backgroundColor};
+    border-radius: 15px 0 15px 15px;
+    margin-left: 5px;
+    padding: 13px;
+    font-size: 18px;
+`;
+
+const SentAt = styled.span`
+    font-size: 12px;
+    opacity: 0.8;
+`;
+
+interface Props extends MessageType {
+    // receiver: string;
+}
+const SentMessage: React.FC<Props> = ({ content, timestamp }) => {
+    const theme = useTheme();
+    return (
+        <Base>
+            <SpeechBubble backgroundColor={theme.colors.primary}>
+                {content}
+            </SpeechBubble>
+            <SentAt>{timestamp}</SentAt>
+        </Base>
+    )
 }
 export default SentMessage;
